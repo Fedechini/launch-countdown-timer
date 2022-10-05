66bac33a494ce2bd9516dcd3f6c0f5a3432d7a4c
Fede Bertoni <mitrero1@gmail.com>
2022-10-06 10:08:37 +1100

    countdown working

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-"use strict";
+("use strict");
+
+// DOM
+const header = document.querySelector(".header");
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+const dateToLaunch = new Date("Dec 27, 2023 22:22:22").getTime();
+
+// countdown
+const countdown = setInterval(() => {
+  const now = new Date().getTime();
+
+  const timeLeft = dateToLaunch - now;
+
+  days.textContent = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  hours.textContent = Math.floor(
+    (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  minutes.textContent = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  seconds.textContent = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  if (timeLeft < 0) {
+    header.innerHTML = "countdown is over!";
+  }
+}, 1000);
